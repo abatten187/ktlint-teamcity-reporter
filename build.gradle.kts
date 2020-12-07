65a729886935da7274db56587a945e4f5539819e
Alexander Batten <alexander@nurturecloud.com>
2020-12-07 20:39:47 +1100

    changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-group = "abatten187.ktlint-teamcity-reporter"
-version = "0.1"
-
-publishing {
-    publications {
-        create<MavenPublication>("ktlintTeamCityReporter") {
-            from(components["java"])
-        }
-    }
-
-    repositories {
-        maven {
-            name = "myRepo"
-            url = uri("file://${buildDir}/repo")
-        }
-    }
-}
-
 plugins {
     kotlin("jvm") version "1.4.10"
-    `maven-publish`
+    `maven`
 }
+
+group = "com.github.abatten187"
+version = "0.1"
 
 repositories {
     mavenCentral()
